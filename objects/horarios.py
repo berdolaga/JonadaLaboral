1ac0aedcacd52377cb6c28f6c273949764c61b47
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:56:00 +0200

    Update horarios.py

--- objects/horarios.py
+++ objects/horarios.py
@@ -1,5 +1,10 @@
 # objects/horarios.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 class Horario:
     def __init__(self,
     id_horario,
