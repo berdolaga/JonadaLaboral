1762ca073aafcaa80b7b0906887e9c16f8f9b739
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:55:50 +0200

    Update fichajes.py

--- objects/fichajes.py
+++ objects/fichajes.py
@@ -1,5 +1,10 @@
 # objects/fichajes.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 class Fichaje:
     def __init__(self,
     id_fichaje,
