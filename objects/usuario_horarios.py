173748b3bc321fa40ea9e4b4104a45acfe910ec4
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:56:19 +0200

    Update usuario_horarios.py

--- objects/usuario_horarios.py
+++ objects/usuario_horarios.py
@@ -1,5 +1,10 @@
 # objects/usuario_horarios.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 class UsuarioHorario:
     def __init__(self,
     id_usuario_horario,
