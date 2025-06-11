2b6bbc246a2550ed468f836dbb7a4596d38ec603
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:55:12 +0200

    Update __init__.py

--- interfaces/__init__.py
+++ interfaces/__init__.py
@@ -1,5 +1,10 @@
 # interfaces/__init__.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 from .telegram_bot import bot
 
 __all__ = ['bot']
