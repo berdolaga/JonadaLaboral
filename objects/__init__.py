f333b562a4abf86a1330dace404b0d8b746d78c7
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:55:35 +0200

    Update __init__.py

--- objects/__init__.py
+++ objects/__init__.py
@@ -1,5 +1,10 @@
 # objects/__init__.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 from .usuarios import User
 from .empresa import Empresa
 from .teclado import Teclado
