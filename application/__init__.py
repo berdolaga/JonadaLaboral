8394b43a631d60ec321a695ffd35747ae155f48f
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:53:29 +0200

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,10 @@
 # application/__init__.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 from .user_servicios_inicializar import *
 from .funciones_telegram import *
 from .manejador_mensajes import *
