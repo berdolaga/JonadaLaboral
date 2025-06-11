c0fe18d22c3c0b8f742139d0409606b66647c3ad
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:56:48 +0200

    Update bot_ports.py

--- ports/bot_ports.py
+++ ports/bot_ports.py
@@ -1,4 +1,9 @@
 # ports/bot_ports.py
+
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
  
 from abc import ABC, abstractmethod
 
