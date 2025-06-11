ecaaebecf524430091822314eb6ea11d17b0c2ca
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:56:40 +0200

    Update __init__.py

--- ports/__init__.py
+++ ports/__init__.py
@@ -1,5 +1,10 @@
 # ports/__init__.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 from .usuario_ports import UserPort
 from .empresa_ports import EmpresaPort
 from .bot_ports import BotPuerto
