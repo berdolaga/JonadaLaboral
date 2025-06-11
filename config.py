cfe17b1daf1c697edc6f4b5f8b24ffed35fc9eae
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:50:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 # config.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 import os
 from dotenv import load_dotenv
 
