82a1b833953241b9d1d5386bb335cbb88ffba9e6
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:54:46 +0200

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,5 +1,10 @@
 # db/__init__.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 from .tables import Session, UserDB, FichajeDB, HorarioDB, Usuario_HorarioDB
 
 __all__ = ['UserDB', 'Session','FichajeDB','HorarioDB', 'Usuario_HorarioDB']
