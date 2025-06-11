825768f7e283a26435744507884ce4654bd360f8
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:52:24 +0200

    Update horarios_adapter.py

--- adapters/horarios_adapter.py
+++ adapters/horarios_adapter.py
@@ -1,6 +1,10 @@
 # adapters/horarios_adapters.py
 
-#from ports.user_repositorio import UserRepositorio
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 from objects.horarios import Horario as Elemento
 from db.tables import HorarioDB as ObjeCTDB, db_session
 
