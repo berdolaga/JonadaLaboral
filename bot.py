d947f2b193858c8c5a842377082f614d1840f5b8
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:49:42 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
 # bot.py
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
 
 from interfaces.telegram_bot import run as telegram_run
 
