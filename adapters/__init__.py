9603cbed9db70c1acb32ddf59024d20bcf3cd962
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:50:54 +0200

    Update __init__.py

--- adapters/__init__.py
+++ adapters/__init__.py
@@ -1,5 +1,10 @@
 # adapters/__init__.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 from .diccionarios import *
 from .telegram_bot_adapter import *
 from .empresa_adapter import EmpresaAdapter
