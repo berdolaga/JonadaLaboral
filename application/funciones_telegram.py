2578cfd8fbf6d2d853b2380a70cf8568193ccbaa
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:53:42 +0200

    Update funciones_telegram.py

--- application/funciones_telegram.py
+++ application/funciones_telegram.py
@@ -1,5 +1,10 @@
 # application/funciones_telegram.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 import sys
 import pdfkit
 from datetime import datetime, timedelta, date
