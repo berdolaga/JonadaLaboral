20a06afeee567ecd8c364850e9b2fa4a7f9627aa
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:56:08 +0200

    Update teclado.py

--- objects/teclado.py
+++ objects/teclado.py
@@ -1,5 +1,10 @@
 # objects/teclado.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 class Teclado:
     def __init__(self, botones, texto):
         self.botones = botones
