d5aa54e4a0bf369ff7e2099408869dac511556be
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:55:43 +0200

    Update empresa.py

--- objects/empresa.py
+++ objects/empresa.py
@@ -1,5 +1,10 @@
 # objects/empresa.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 class Empresa:
     def __init__(self,
     id_empresa,
