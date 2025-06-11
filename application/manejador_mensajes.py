8db67756caa69e66ee33933249c3a064d84e0028
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:54:11 +0200

    Update manejador_mensajes.py

--- application/manejador_mensajes.py
+++ application/manejador_mensajes.py
@@ -1,5 +1,9 @@
 # application/manejador_mensajes.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
 
 from application.menus_telegram import MenuTelegram
 from application.funciones_telegram import FuncionTelegram
