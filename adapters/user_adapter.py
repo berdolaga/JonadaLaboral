629383e0e0873ca27bf23122659c83bb1fe9dede
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:52:56 +0200

    Update user_adapter.py

--- adapters/user_adapter.py
+++ adapters/user_adapter.py
@@ -1,6 +1,10 @@
 # adapters/user_adapters.py
 
-#from ports.user_repositorio import UserRepositorio
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 from objects.usuarios import User
 from db.tables import UserDB, db_session
 
