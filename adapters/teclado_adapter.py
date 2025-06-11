812e844b650356d7ee85e921b293118e2e50bbae
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:52:35 +0200

    Update teclado_adapter.py

--- adapters/teclado_adapter.py
+++ adapters/teclado_adapter.py
@@ -1,5 +1,10 @@
 # adapters/teclado_adapters.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 from objects.teclado import Teclado
 
 class TecladoAdapter():
