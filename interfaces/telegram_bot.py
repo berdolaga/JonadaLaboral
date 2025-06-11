768f5f82e560dd3925c6295edaf01bf3457f8020
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:55:23 +0200

    Update telegram_bot.py

--- interfaces/telegram_bot.py
+++ interfaces/telegram_bot.py
@@ -1,5 +1,10 @@
 # interfaces/telegram_bot.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 import telebot
 import time
 import threading
