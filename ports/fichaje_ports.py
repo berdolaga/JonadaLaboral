b1521fa982770ba578d95e1bf07793f0c02ce6f4
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:57:05 +0200

    Update fichaje_ports.py

--- ports/fichaje_ports.py
+++ ports/fichaje_ports.py
@@ -1,5 +1,10 @@
 # ports/fichaje_ports.py
 
+# Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
+# Curso de Adaptación al Grado en Ingeniería Informática - UNIR
+# Autor: [Alberto Toledo Escrihuela]
+# Año: 2025
+
 from adapters.fichaje_adapter import FichajeAdapter as Adaptador
 from objects.fichajes import Fichaje as Elemento
 
