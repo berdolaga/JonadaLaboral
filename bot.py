b3431794dae06a5a855487718c8d62e2c784e29a
berdolaga <57809304+berdolaga@users.noreply.github.com>
2025-06-11 16:50:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 # bot.py
+
 # Proyecto desarrollado como parte del Trabajo de Fin de Grado (TFG)
 # Curso de Adaptación al Grado en Ingeniería Informática - UNIR
 # Autor: [Alberto Toledo Escrihuela]
